d4136c26c0729b115dfbc9de54c2be8af1449eab
mirror12k <mirror12k@gmail.com>
2024-12-01 23:19:20 -0500

    - improvements

--- toolkit.pm
+++ toolkit.pm
@@ -9,6 +9,7 @@ sub product { my $p = 1; foreach my $n (@_) { $p *= $n } $p }
 sub min { (sort { $a <=> $b } @_)[0] }
 sub max { (sort { $a <=> $b } @_)[-1] }
 sub reduce (&$@) { my ($fun, $reduced, @values) = @_; for (@values) { $reduced = $fun->($reduced, $_) } $reduced }
+sub freq { my %freq; $freq{$_}++ for @_; return %freq; }
 sub all (&@) { my ($fun, @args) = @_; foreach (@args) { return 0 unless $fun->() } return 1 }
 sub none (&@) { my ($fun, @args) = @_; foreach (@args) { return 0 if $fun->() } return 1 }
 # n-dimensional mapper function
@@ -39,6 +40,16 @@ sub map_nd_indexed {
 	$iter //= $arr;
 	return [ map $fun->($arr, [ @coords, $_ ]), 0 .. $#$iter ] if (@$iter >= 0 and ref $iter->[0] ne 'ARRAY');
 	return [ map map_nd_indexed($fun, $arr, $iter->[$_], @coords, $_), 0 .. $#$iter ];
+}
+
+sub parse_2d_string_array {
+    my ($input) = @_;
+    return [ map [ split ], grep $_, split /\n/, $input ];
+}
+
+sub transpose_2d (@) {
+    my ($arr) = @_;
+    return [ map { my $i = $_; [ map $_->[$i], @$arr ] } 0 .. $#{$arr->[0]} ]
 }
 
 
